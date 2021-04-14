3f3bd8206bb64b8729cb57033c880030252c4591
Dillan Spencer <46987282+DillanSpencer@users.noreply.github.com>
2021-04-13 17:53:13 -0700

    Update

--- Ray-Trace/main.py
+++ Ray-Trace/main.py
@@ -1,6 +1,82 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from structs import Sphere
+import re
+import sys
+from structs import *
+
+width = 300
+height = 200
+
+max_depth = 3
+
+camera = np.array([0, 0, 1])
+ratio = float(width) / height
+screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
+
+
+def main():
+    file = sys.argv[1]
+    f = open(file, "r")
+    lines = f.readlines()
+    scene = parse(lines)
+    objects = [s.output() for s in scene.spheres]
+    lights = [s.output() for s in scene.lights]
+    image = np.zeros((height, width, 3))
+    for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
+        for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
+            # screen is on origin
+            pixel = np.array([x, y, 0])
+            origin = camera
+            direction = normalize(pixel - origin)
+
+            color = np.zeros((3))
+            reflection = 1
+
+            for light in lights:
+                for k in range(max_depth):
+                    # check for intersections
+                    nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
+                    if nearest_object is None:
+                        break
+
+                    intersection = origin + min_distance * direction
+                    normal_to_surface = normalize(intersection - nearest_object['center'])
+                    shifted_point = intersection + 1e-5 * normal_to_surface
+
+                    intersection_to_light = normalize(light['position'] - shifted_point)
+
+                    _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
+                    intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
+                    is_shadowed = min_distance < intersection_to_light_distance
+
+                    if is_shadowed:
+                        break
+
+                    illumination = np.zeros((3))
+
+                    # ambiant
+                    illumination += nearest_object['ambient'] * light['ambient']
+
+                    # diffuse
+                    illumination += nearest_object['diffuse'] * light['diffuse'] * np.dot(intersection_to_light,
+                                                                                          normal_to_surface)
+
+                    # specular
+                    intersection_to_camera = normalize(camera - intersection)
+                    H = normalize(intersection_to_light + intersection_to_camera)
+                    illumination += nearest_object['specular'] * light['specular'] * np.dot(normal_to_surface, H) ** (
+                            nearest_object['shininess'] / 4)
+
+                    # reflection
+                    color += reflection * illumination
+                    reflection *= nearest_object['reflection']
+
+                    origin = shifted_point
+                    direction = reflected(direction, normal_to_surface)
+
+                image[i, j] = np.clip(color, 0, 1)
+        print("%d/%d" % (i + 1, height))
+
+    plt.imsave('image.png', image)
 
 
 def normalize(vector):
@@ -34,80 +110,95 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
     return nearest_object, min_distance
 
 
-width = 300
-height = 200
+def parse(lines):
+    near = None
+    left = None
+    right = None
+    bottom = None
+    top = None
+    res = None
+    spheres = []
+    lights = []
+    back = None
+    ambient = None
+    output = None
 
-max_depth = 3
+    for line in lines:
+        line = line.strip()
+        # print(line)
+        if re.search("^NEAR", line):
+            near = re.search("[+-]?\d+(?:\.\d+)?", line).group(0)
+        elif re.search("^LEFT", line):
+            left = re.search("[+-]?\d+(?:\.\d+)?", line).group(0)
+        elif re.search("^RIGHT", line):
+            right = re.search("[+-]?\d+(?:\.\d+)?", line).group(0)
+        elif re.search("^BOTTOM", line):
+            bottom = re.search("[+-]?\d+(?:\.\d+)?", line).group(0)
+        elif re.search("^TOP", line):
+            top = re.search("[+-]?\d+(?:\.\d+)?", line).group(0)
+        elif re.search("^RES", line):
+            res = re.findall("[+-]?\d+(?:\.\d+)?", line)
+            res[0] = int(res[0])
+            res[1] = int(res[1])
+        elif re.search("^SPHERE", line):
+            info = line.split()
+            name = info[1]
+            posx = float(info[2])
+            posy = float(info[3])
+            posz = float(info[4])
+            sclx = float(info[5])
+            scly = float(info[6])
+            sclz = float(info[7])
+            r = float(info[8])
+            g = float(info[9])
+            b = float(info[10])
+            ka = float(info[11])
+            kd = float(info[12])
+            ks = float(info[13])
+            kr = float(info[14])
+            n = float(info[15])
+            sphere = Sphere(name)
+            sphere.set_position(posx, posy, posz)
+            sphere.set_scale(sclx, scly, sclz)
+            sphere.set_color(r, g, b)
+            sphere.set_diffusion(kd)
+            sphere.set_specular(ks)
+            sphere.set_reflection(kr)
+            spheres.append(sphere)
 
-camera = np.array([0, 0, 1])
-ratio = float(width) / height
-screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
+        elif re.search("^LIGHT", line):
+            info = line.split()
+            name = info[1]
+            posx = float(info[2])
+            posy = float(info[3])
+            posz = float(info[4])
+            lr = float(info[5])
+            lg = float(info[6])
+            lb = float(info[7])
+            lght = Light(name, posx, posy, posz, lr, lg, lb)
+            lights.append(lght)
 
-light = {'position': np.array([5, 5, 5]), 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]),
-         'specular': np.array([1, 1, 1])}
+        elif re.search("^BACK", line):
+            info = line.split()
+            r = float(info[1])
+            g = float(info[2])
+            b = float(info[3])
+            back = [r, g, b]
 
-sphere_one = Sphere("one")
-sphere_one.set_position(-0.2, 0, -1)
-sphere_one.set_color(np.array([0.1, 0, 0]))
-sphere_one.set_scale(1, 1, 1)
-sphere_one.set_other(np.array([0.1, 0, 0]), np.array([0.7, 0, 0]), np.array([1, 1, 1]), 0.5)
+        elif re.search("^AMBIENT", line):
+            info = line.split()
+            lr = float(info[1])
+            lg = float(info[2])
+            lb = float(info[3])
+            ambient = [lr, lg, lb]
 
-objects = [{'center': np.array([0, -9000, 0]), 'radius': 9000 - 0.7, 'ambient': np.array([0.1, 0.1, 0.1]),
-            'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-           sphere_one.output()]
+        elif re.search("^OUTPUT", line):
+            info = line.split()
+            output = info[1]
 
-image = np.zeros((height, width, 3))
-for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
-    for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
-        # screen is on origin
-        pixel = np.array([x, y, 0])
-        origin = camera
-        direction = normalize(pixel - origin)
+    scene = Scene(near, left, right, bottom, top, res, spheres, lights, back, ambient, output)
+    return scene
 
-        color = np.zeros((3))
-        reflection = 1
 
-        for k in range(max_depth):
-            # check for intersections
-            nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
-            if nearest_object is None:
-                break
-
-            intersection = origin + min_distance * direction
-            normal_to_surface = normalize(intersection - nearest_object['center'])
-            shifted_point = intersection + 1e-5 * normal_to_surface
-            intersection_to_light = normalize(light['position'] - shifted_point)
-
-            _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
-            intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
-            is_shadowed = min_distance < intersection_to_light_distance
-
-            if is_shadowed:
-                break
-
-            illumination = np.zeros((3))
-
-            # ambiant
-            illumination += nearest_object['ambient'] * light['ambient']
-
-            # diffuse
-            illumination += nearest_object['diffuse'] * light['diffuse'] * np.dot(intersection_to_light,
-                                                                                  normal_to_surface)
-
-            # specular
-            intersection_to_camera = normalize(camera - intersection)
-            H = normalize(intersection_to_light + intersection_to_camera)
-            illumination += nearest_object['specular'] * light['specular'] * np.dot(normal_to_surface, H) ** (
-                    nearest_object['shininess'] / 4)
-
-            # reflection
-            color += reflection * illumination
-            reflection *= nearest_object['reflection']
-
-            origin = shifted_point
-            direction = reflected(direction, normal_to_surface)
-
-        image[i, j] = np.clip(color, 0, 1)
-    print("%d/%d" % (i + 1, height))
-
-plt.imsave('image.png', image)
+if __name__ == '__main__':
+    main()
--- Ray-Trace/structs.py
+++ Ray-Trace/structs.py
@@ -28,8 +28,8 @@ class Sphere:
         self.scale_y = sy
         self.scale_z = sz
 
-    def set_color(self, color):
-        self.color = color
+    def set_color(self, r, g, b):
+        self.ka = np.array([r, g, b])
 
     def set_diffusion(self, diff):
         self.kd = diff
@@ -50,3 +50,92 @@ class Sphere:
         return {'center': np.array([self.x, self.y, self.z]),
                 'radius': 0.7, 'ambient': self.ka, 'diffuse': self.kd,
                 'specular': self.ks, 'shininess': 100, 'reflection': self.kr}
+
+
+class Light:
+    name = None
+    posx = 0
+    posy = 0
+    posz = 0
+    lr = 0
+    lg = 0
+    lb = 0
+
+    def __init__(self, name, posx, posy, posz, lr, lg, lb):
+        self.name = name
+        self.posx = posx
+        self.posy = posy
+        self.posz = posz
+        self.lr = lr
+        self.lg = lg
+        self.lb = lb
+
+    def set_position(self, x, y, z):
+        self.posx = x
+        self.posy = y
+        self.posz = z
+
+    def set_color(self, r, g, b):
+        self.lr = r
+        self.lg = g
+        self.lb = b
+
+    def output(self):
+        return {'position': np.array([self.posx, self.posy, self.posz]),
+                'ambient': np.array([self.lr, self.lg, self.lb]), 'diffuse': np.array([1, 1, 1]),
+                'specular': np.array([1, 1, 1])}
+
+    def print_light(self):
+        print("LIGHTS:")
+        print("name:", self.name)
+        print("posx:", self.posx)
+        print("posy:", self.posy)
+        print("posz:", self.posz)
+        print("lr:", self.lr)
+        print("lg:", self.lg)
+        print("lb:", self.lb)
+
+
+class Scene:
+    near = None
+    left = None
+    right = None
+    bottom = None
+    top = None
+    res = None
+    spheres = []
+    lights = []
+    back = None
+    ambient = None
+    output = None
+
+    def __init__(self, near, left, right, bottom, top, res, spheres, lights, back, ambient, output):
+        self.near = near
+        self.left = left
+        self.right = right
+        self.bottom = bottom
+        self.top = top
+        self.res = res
+        self.spheres = spheres
+        self.lights = lights
+        self.back = back
+        self.ambient = ambient
+        self.output = output
+
+    def print_scene(self):
+        print("NEAR:", self.near)
+        print("LEFT:", self.left)
+        print("RIGHT:", self.right)
+        print("BOTTOM:", self.bottom)
+        print("TOP:", self.top)
+        print("RES:", self.res)
+        for sphere in self.spheres:
+            sphere.print_sphere()
+
+        for light in self.lights:
+            light.print_light()
+
+        print("BACK:", self.back)
+        print("AMBIENT:", self.ambient)
+        print("OUTPUT:", self.output)
+
